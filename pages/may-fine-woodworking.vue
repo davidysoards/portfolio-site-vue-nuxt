5bece71f83cc44989f6f14180035512336bc7711
David Soards <davidysoards@gmail.com>
2019-12-15 13:46:18 -0500

    Add MFW project page to contentful

--- pages/may-fine-woodworking.vue
+++ pages/may-fine-woodworking.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="project-page">
     <section class="hero">
-      <div class="wrapper project-hero-grid">
+      <div class="hero-wrapper project-hero-grid">
         <div class="hero-heading">
           <h1 class="animated fadeInLeftBig">Case Study</h1>
         </div>
         <img
-          src="~/assets/img/may-fine-vertical-trans.png"
+          :src="heroImage"
           class="project-image animated rotateIn delay-5"
-          alt="may fine woodworking logo"
+          :alt="heroAlt"
         />
 
         <arrow-double-down />
@@ -17,186 +17,9 @@
 
     <main id="scroll-down" class="projects">
       <div class="wrapper">
-        <h2>May Fine Woodworking</h2>
-
-        <p>
-          In April of 2017, I received a call from Eric May. Eric is an amazing
-          custom cabinetmaker, and his business, May Fine Woodworking, was in
-          need of a new website. His site from 2012 through Wix.com was no
-          longer adequate. I told him that I would be glad to help. I didn’t
-          know it at the time, but this project turned into a complete redesign
-          of the visual brand identity for May Fine Woodworking.
-        </p>
-
-        <h3>Business Needs &amp; Goals for the Website</h3>
-        <p>
-          I met with Eric to learn more about his business and to find out
-          exactly what the needs were for his website. I learned that most of
-          his business came through word of mouth, and that has been
-          surprisingly effective for him. He told me that he prides himself on
-          telling the truth, doing quality work, and being on time. Such a
-          striking combination of qualities is not to be taken for granted.
-          Highlighting my client's values and showcasing his beautiful handmade
-          products became my goal for his website.
-        </p>
-
-        <figure>
-          <img
-            src="~/assets/img/MFW-old-home-page.jpg"
-            alt="screenshot of the OLD mayfinewoodworking.com home page"
-          />
-          <figcaption>
-            The old MFW website was in dire need of an update.
-          </figcaption>
-        </figure>
-
-        <h3>Photography</h3>
-        <p>
-          We identified May Fine Woodworking's target audience as interior
-          designers, realtors, homeowners, and also builders. They would want to
-          see “pictures, lots of pictures,” as Eric put it. Unfortunately, he
-          was not happy with the quality of his current photos. They had been
-          taken with an old camera phone in less than ideal lighting almost
-          immediately after the jobs were finished. This meant that the shelves,
-          cabinets, and the rooms in which the photos were taken were empty.
-          Much like a home needs to be staged for showing by a realtor, pictures
-          of finished woodwork have more appeal when set in the context of a
-          home with real household objects. Eric asked whether I knew any
-          photographers, and as a matter of fact I did. Me!
-        </p>
-
-        <p>
-          Another of my Mr. May's admirable characteristics is that he keeps in
-          touch with most of his clients, many of whom live in this area. We
-          arranged to meet with a few of them, so I could take photographs for
-          the website. In addition, Eric showed me his garage which he had
-          converted into a really cool wood workshop. I decided that pictures of
-          my client working in the shop would make excellent background images
-          and banners for his new, renovated website.
-        </p>
-
-        <figure>
-          <a
-            href="~/assets/img/MFW-home-screenshot-FULL.jpg"
-            data-lightbox="may-fine-1"
-            data-title="May Fine Woodworking home page"
-          >
-            <img
-              src="~/assets/img/mfw-home-thumb.jpg"
-              alt="May Fine Woodworking home page"
-            />
-          </a>
-          <figcaption>
-            My goal was to showcase Eric's values and his beautiful handmade
-            products.
-          </figcaption>
-        </figure>
-
-        <h3>Organizing the Website</h3>
-        <p>
-          Instead of having one page for testimonials and another page with
-          pictures of the cabinetry, we decided to have separate pages for each
-          project. This way the pages can be labeled with the homeowners’ names,
-          and their testimonials can be displayed along with the pictures of the
-          items of woodwork in each home. The endorsements are effective and
-          seem believable because they are displayed alongside of pictures of
-          the work to which they are referring. I organized the Project pages
-          together in a dropdown menu that appears when site visitors hover over
-          or tap on “Projects” in the main navigation. This architecture allows
-          for new projects to be added to the website by simply creating a new
-          project page and adding it to the drop down menu.
-        </p>
-
-        <figure>
-          <a
-            href="~/assets/img/MFW-lewis-screenshot-FULL.jpg"
-            data-lightbox="may-fine-2"
-            data-title="May Fine Woodworking full project page"
-          >
-            <img
-              src="~/assets/img/mfw-ashley-thumb.jpg"
-              alt="May Fine Woodworking Ashley Kitchen page"
-            />
-          </a>
-          <figcaption>
-            Pictures and the corresponding testimonials are showcased together
-            as projects.
-          </figcaption>
-        </figure>
-
-        <h3>Logo Design</h3>
-        <p>
-          May Fine Woodworking's old logo fit well with the old site, but since
-          we were updating the look of their web presence, it was time to update
-          the logo as well. I was happy to tackle the design of a logo along
-          with constructing the website and doing the photography. The new
-          design is reminiscent of the old, but with a much more contemporary
-          and balanced look. The improvements include the use of brighter shades
-          of green and gold, a much fuller and more lively maple tree emblem,
-          and an updated choice of fonts. Together these changes make for a
-          much-improved visual presence.
-        </p>
-
-        <figure>
-          <img
-            src="~/assets/img/MFW-old-vs-new-logos.png"
-            alt="may fine woodworking old vs. new logo"
-          />
-          <figcaption>
-            A new emblem, brighter colors, and updated font choices make for a
-            much more inviting logo design.
-          </figcaption>
-        </figure>
-
-        <p>
-          By using the same green and gold hues and the same font families
-          throughout the website and in the logo design, I was able to bring a
-          sense of unity to the brand identity of May Fine Woodworking. This
-          coordination was accomplished by accessing Squarespace’s built-in
-          Typekit integration. The font families used are Sofia and Freight.
-        </p>
-
-        <h3>Switching Platforms</h3>
-        <p>
-          During my initial meeting with my client, I suggested that he switch
-          his site from Wix.com to Squarespace, and he was more than happy to do
-          so. With Squarespace’s fantastic browser-based site building tools, I
-          was able efficiently to put together a very professional looking
-          website with a contemporary responsive design. Adding additional
-          projects and content and making changes to the site in the future will
-          also be streamlined thanks to the Squarespace platform. Another
-          benefit is the ability to transfer ownership quickly once the design
-          is approved and payment is received.
-        </p>
-
-        <figure>
-          <a
-            href="~/assets/img/mfw-home-full-mobile.JPG"
-            data-lightbox="may-fine-4"
-            data-title="Full mobile screenshot"
-          >
-            <img
-              src="~/assets/img/MFW-Ashley-mobile-screenshot.jpg"
-              alt="may fine woodworking on mobile"
-            />
-          </a>
-          <figcaption>
-            The responsive design and looks great on mobile devices too.
-          </figcaption>
-        </figure>
-
-        <p>
-          My thanks go out to Eric for bringing me this wonderful opportunity to
-          create both a showcase for his amazing handcrafted cabinets and a
-          visual identity for his brand.
-        </p>
-        <p>
-          Click or tap
-          <a href="https://www.mayfinewoodworking.com/" target="_blank">here</a>
-          to visit the website and view Eric's work.
-        </p>
+        <h2>{{ pageTitle }}</h2>
+        <div class="rich-text" v-html="richText"></div>
       </div>
-      <!-- /wrapper -->
 
       <div>
         <a href="#scroll-down">
@@ -208,12 +31,42 @@
 </template>
 
 <script>
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { createClient } from '../plugins/contentful';
 import ArrowDoubleDown from '~/components/ArrowDoubleDown.vue';
+
+const client = createClient();
+const richTextRenderOptions = {
+  renderNode: {
+    'embedded-asset-block': node =>
+      `<figure style="margin-bottom:2.6rem; text-align:center;">
+        <img src="${node.data.target.fields.file.url}" alt="" />
+        <figcaption style="font-size:1rem;">${node.data.target.fields.description}</figcaption>
+      </figure>`,
+  },
+};
 
 export default {
   name: 'MayFineWoodworking',
   components: {
     ArrowDoubleDown,
+  },
+  asyncData({ env }) {
+    return client
+      .getEntry('7jMnJrE8kLcYXGJjm9xcSJ')
+      .then(entry => {
+        // console.log(entry.fields);
+        return {
+          heroImage: entry.fields.heroImage.fields.file.url,
+          heroAlt: entry.fields.heroImage.fields.description,
+          pageTitle: entry.fields.pageTitle,
+          richText: documentToHtmlString(
+            entry.fields.pageBody,
+            richTextRenderOptions
+          ),
+        };
+      })
+      .catch(console.error);
   },
 };
 </script>
@@ -262,17 +115,18 @@ export default {
   filter: drop-shadow(3px 3px 3px rgba(0, 0, 0, 0.2));
 }
 
-main.projects {
-  .wrapper {
-    max-width: 45em;
-  }
-  h2,
-  h3 {
-    text-align: left;
-  }
-  p {
-    font-size: 1.3em;
-    text-align: left;
-  }
+.wrapper {
+  max-width: 740px;
+}
+
+h2 {
+  text-align: left;
+  margin-bottom: 1em;
+}
+
+.rich-text {
+  text-align: left;
+  font-size: 1.1em;
+  line-height: 1.5;
 }
 </style>
