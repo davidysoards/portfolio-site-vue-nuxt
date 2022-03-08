dc7de13ca05d626d475bff1d0ad9ee6e23709bc1
David Soards <david@soards.me>
2022-03-07 19:09:57 -0500

    Fix Contact Link in Footer

--- components/TheStickyNav.vue
+++ components/TheStickyNav.vue
@@ -13,7 +13,7 @@
             <nuxt-link to="/about">About</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="#contact">Contact</nuxt-link>
+            <nuxt-link to="/contact">Contact</nuxt-link>
           </li>
         </ul>
       </nav>
